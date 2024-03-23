ac570bde2411b18a3b01a8abafa892b405c30490
AIPhD <sbilaj@gmail.com>
2024-03-23 18:27:18 +0100

    maze implemented

--- gym_examples/envs/grid_world.py
+++ gym_examples/envs/grid_world.py
@@ -1,13 +1,136 @@
+import random
 import gym
 from gym import spaces
 import pygame
 import numpy as np
+import config as c
 
+
+def create_maze(size=c.SIZE):
+    '''Build random maze within grid world. algorithm works with uneven number of cells.'''
+
+    maze = np.zeros((size, size))
+    cell_list = []
+    maze_list = []
+    sample_walls = []
+
+    for i in range(size):
+        for j in range(size):
+
+            if (i + 1)%2 == 0 or (j + 1)%2 == 0:
+                maze[i][j] = -2
+
+            else:
+                cell_list.append([i, j])
+
+    init_cell_position = random.randrange(len(cell_list))
+    init_cell = cell_list[init_cell_position]
+    maze_list.append(init_cell)
+    del cell_list[init_cell_position]
+
+    if init_cell[0] > 0:
+        sample_walls.append([init_cell[0] - 1, init_cell[1]])
+        maze[init_cell[0] - 1][init_cell[1]] = -1
+
+    if init_cell[1] > 0:
+        sample_walls.append([init_cell[0], init_cell[1] - 1])
+        maze[init_cell[0]][init_cell[1] - 1] = -1
+
+    if init_cell[0]  < c.SIZE - 1:
+        sample_walls.append([init_cell[0] + 1, init_cell[1]])
+        maze[init_cell[0] + 1][init_cell[1]] = -1
+
+    if init_cell[1] < c.SIZE -1:
+        sample_walls.append([init_cell[0], init_cell[1] + 1])
+        maze[init_cell[0]][init_cell[1] + 1] = -1
+
+    print(maze)
+    print('hello')
+
+    while len(sample_walls) > 0:
+        new_cell_list_position = random.randrange(len(sample_walls))
+        loc_1 = [sample_walls[new_cell_list_position][0] + 1,
+                    sample_walls[new_cell_list_position][1]]
+        loc_2 = [sample_walls[new_cell_list_position][0] - 1,
+                    sample_walls[new_cell_list_position][1]]
+        loc_3 = [sample_walls[new_cell_list_position][0],
+                    sample_walls[new_cell_list_position][1] - 1]
+        loc_4 = [sample_walls[new_cell_list_position][0],
+                    sample_walls[new_cell_list_position][1] + 1]
+
+        if loc_1 in cell_list:
+            new_maze_cell_position = cell_list.index(loc_1)
+        elif loc_2 in cell_list:
+            new_maze_cell_position = cell_list.index(loc_2)
+        elif loc_3 in cell_list:
+            new_maze_cell_position = cell_list.index(loc_3)
+        elif loc_4 in cell_list:
+            new_maze_cell_position = cell_list.index(loc_4)
+        else:
+            new_maze_cell_position = None
+
+        if new_maze_cell_position is not None:
+            wall_to_cell_x = sample_walls[new_cell_list_position][0]
+            wall_to_cell_y = sample_walls[new_cell_list_position][1]
+            cell_to_cell_x = cell_list[new_maze_cell_position][0]
+            cell_to_cell_y = cell_list[new_maze_cell_position][1]
+            maze[wall_to_cell_x][wall_to_cell_y] = 0
+            maze_list.append([sample_walls[new_cell_list_position]])
+            maze_list.append(cell_list[new_maze_cell_position])
+
+            if wall_to_cell_x < size - 1:
+                if maze[wall_to_cell_x + 1][wall_to_cell_y] == -2:
+                    sample_walls.append([wall_to_cell_x + 1, wall_to_cell_y])
+                    maze[wall_to_cell_x + 1][wall_to_cell_y] = -1
+
+            if wall_to_cell_x > 0:
+                if maze[wall_to_cell_x - 1][wall_to_cell_y] == -2:
+                    sample_walls.append([wall_to_cell_x - 1, wall_to_cell_y])
+                    maze[wall_to_cell_x - 1][wall_to_cell_y] = -1
+
+            if wall_to_cell_y < size - 1:
+                if maze[wall_to_cell_x][wall_to_cell_y + 1] == -2:
+                    sample_walls.append([wall_to_cell_x, wall_to_cell_y + 1])
+                    maze[wall_to_cell_x][wall_to_cell_y + 1] = -1
+
+            if wall_to_cell_y > 0:
+                if maze[wall_to_cell_x][wall_to_cell_y - 1] == -2:
+                    sample_walls.append([wall_to_cell_x, wall_to_cell_y - 1])
+                    maze[wall_to_cell_x][wall_to_cell_y - 1] = -1
+
+            if cell_to_cell_x < size -1:
+                if maze[cell_to_cell_x + 1][cell_to_cell_y] == -2:
+                    sample_walls.append([cell_to_cell_x + 1, cell_to_cell_y])
+                    maze[cell_to_cell_x + 1][cell_to_cell_y] = -1
+
+            if cell_to_cell_x > 0:
+                if maze[cell_to_cell_x - 1][cell_to_cell_y] == -2:
+                    sample_walls.append([cell_to_cell_x - 1, cell_to_cell_y])
+                    maze[cell_to_cell_x - 1][cell_to_cell_y] = -1
+
+            if cell_to_cell_y < size - 1:
+                if maze[cell_to_cell_x][cell_to_cell_y + 1] == -2:
+                    sample_walls.append([cell_to_cell_x, cell_to_cell_y + 1])
+                    maze[cell_to_cell_x][cell_to_cell_y + 1] = -1
+
+            if cell_to_cell_y > 0:
+                if maze[cell_to_cell_x][cell_to_cell_y - 1] == -2:
+                    sample_walls.append([cell_to_cell_x, cell_to_cell_y - 1])
+                    maze[cell_to_cell_x][cell_to_cell_y - 1] = -1
+
+            del cell_list[new_maze_cell_position]
+
+        del sample_walls[new_cell_list_position]
+        # print(maze)
+
+    return maze, list(np.asarray(np.where(maze<0)).T.tolist())
+
+MAZE, WALL_LIST = create_maze()
 
 class GridWorldEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def __init__(self, render_mode=None, size=8):
+    def __init__(self, render_mode=None, size=c.SIZE, maze=MAZE, wall_list=WALL_LIST):
         self.size = size  # The size of the square grid
         self.window_size = 512  # The size of the PyGame window
 
@@ -17,8 +140,13 @@ class GridWorldEnv(gym.Env):
             {
                 "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
                 "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "walls": spaces.Box(0, size - 1, shape=(2,), dtype=int)
             }
         )
+
+        self.maze=MAZE
+        self.wall_list = WALL_LIST
+        print(self.maze)
 
         # We have 4 actions, corresponding to "right", "up", "left", "down", "right"
         self.action_space = spaces.Discrete(4)
@@ -49,7 +177,9 @@ class GridWorldEnv(gym.Env):
         self.clock = None
 
     def _get_obs(self):
-        return {"agent": self._agent_location, "target": self._target_location}
+        return {"agent": self._agent_location,
+                "target": self._target_location,
+                "walls": self.wall_list}
 
     def _get_info(self):
         return {
@@ -62,15 +192,19 @@ class GridWorldEnv(gym.Env):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
-        # Choose the agent's location uniformly at random
-        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
 
-        # We will sample the target's location randomly until it does not coincide with the agent's location
-        self._target_location = self._agent_location
-        while np.array_equal(self._target_location, self._agent_location):
-            self._target_location = self.np_random.integers(
-                0, self.size, size=2, dtype=int
-            )
+        self._target_location = np.asarray([self.size - 1, self.size - 1])
+        # Choose the agent's location uniformly at random within the maze
+        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        while self._agent_location.tolist() in self.wall_list and np.array_equal(self._target_location, self._agent_location):
+            self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+
+        # We will sample the target's location randomly until it does not coincide with the agent's location or the walls
+        # self._target_location = self._agent_location
+        # while np.array_equal(self._target_location, self._agent_location) or self._target_location.tolist() in self.wall_list:
+        #     self._target_location = self.np_random.integers(
+        #         0, self.size, size=2, dtype=int
+        #     )
 
         observation = self._get_obs()
         info = self._get_info()
@@ -82,17 +216,21 @@ class GridWorldEnv(gym.Env):
 
     def step(self, action):
         # Map the action (element of {0,1,2,3}) to the direction we walk in
+        prev_location = self._agent_location
         direction = self._action_to_direction[action]
-        previous_location = self._action_to_direction
         # We use `np.clip` to make sure we don't leave the grid
         self._agent_location = np.clip(
             self._agent_location + direction, 0, self.size - 1
         )
+
+        if self._agent_location.tolist() in self.wall_list:
+            self._agent_location = self._agent_location - direction
+
         # An episode is done iff the agent has reached the target
         terminated = np.array_equal(self._agent_location, self._target_location)
-        reward = 1 if terminated else -0.01  # Binary sparse rewards
-        if np.array_equal(self._agent_location, previous_location):
-            reward = -0.1
+        reward = 10 if terminated else -0.01  # Binary sparse rewards
+        if not terminated and np.array_equal(self._agent_location, prev_location):
+            reward = -0.5
         observation = self._get_obs()
         info = self._get_info()
 
@@ -134,6 +272,16 @@ class GridWorldEnv(gym.Env):
             (0, 0, 255),
             (self._agent_location + 0.5) * pix_square_size,
             pix_square_size / 3,
+        )
+
+        for wall in self.wall_list:
+            wall = np.asarray(wall)
+            pygame.draw.rect(canvas,
+                             (0, 255, 0),
+                             pygame.Rect(
+                                pix_square_size * wall,
+                                (pix_square_size, pix_square_size),
+            )
         )
 
         # Finally, add some gridlines
