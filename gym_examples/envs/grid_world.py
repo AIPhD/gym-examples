12500da38a5fa22bbb472d97a723f214ed7b9ea3
AIPhD <sbilaj@gmail.com>
2024-11-01 17:09:17 +0100

    env changes

--- gym_examples/envs/grid_world.py
+++ gym_examples/envs/grid_world.py
@@ -22,7 +22,7 @@ CUSTOM_MAZE_5 = np.asarray([[ 0.,  0.,  0.,  0.,  2.],
                             [-1., -1.,  0.,  0,   0.],
                             [ 0.,  0.,  2, -1.,  0.]])
 
-CUSTOM_WALL_LIST_5 = list(np.asarray(np.where(CUSTOM_MAZE_5<0)).T.tolist())
+CUSTOM_WALL_LIST_5 = list(np.asarray(np.where(CUSTOM_MAZE_5==-1)).T.tolist())
 CUSTOM_COIN_LIST_5 = list(np.asarray(np.where(CUSTOM_MAZE_5==2)).T.tolist())
 CUSTOM_TRAP_LIST_5 = list(np.asarray(np.where(CUSTOM_MAZE_5==-2)).T.tolist())
 
@@ -142,17 +142,56 @@ def create_maze(size=c.SIZE):
         del sample_walls[new_cell_list_position]
         # print(maze)
 
-    walls = list(np.asarray(np.where(maze==-1)).T.tolist())
-    coins = list(np.asarray(np.where(maze==2)).T.tolist())
-    traps = list(np.asarray(np.where(maze==-2)).T.tolist())
+    return maze
+
+
+def create_environment(game, size=c.SIZE):
+
+    obj_list = []
+
+    for i in range(0, c.SIZE):
+        for j in range(0, c.SIZE):
+            obj_list.append(np.asarray([i, j]).tolist())
+
+    obj_list.remove(np.asarray([0, 0]).tolist())
+    obj_list.remove(np.asarray([c.SIZE-1, c.SIZE-1]).tolist())
+
+    if game[2] == 1:
+        maze = create_maze(size)
+        walls = list(np.asarray(np.where(maze==-1)).T.tolist())
+        for wall in walls:
+            obj_list.remove(wall)
+
+    else:
+        walls = []
+
+    if game[0] == 1:
+        coins = random.sample(obj_list, 3)
+
+        for coin in coins:
+            obj_list.remove(coin)
+
+    else:
+        coins = []
+
+
+    if game[1] == 1:
+        traps = random.sample(obj_list, 2)
+
+        for trap in traps:
+            obj_list.remove(trap)
+
+    else:
+        traps = []
+
     return walls, coins, traps
 
-WALL_LIST, COIN_LIST, TRAP_LIST = create_maze()
+WALL_LIST, COIN_LIST, TRAP_LIST = create_environment([1, 1, 1])
 
 class GridWorldEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def __init__(self, render_mode=None, size=c.SIZE, wall_list=WALL_LIST, coin_list=COIN_LIST, new_maze=False):
+    def __init__(self, game=[1, 0, 0], render_mode=None, size=c.SIZE, wall_list=WALL_LIST, coin_list=COIN_LIST, new_maze=True):
         self.size = size  # The size of the square grid
         self.window_size = 512  # The size of the PyGame window
 
@@ -169,10 +208,10 @@ class GridWorldEnv(gym.Env):
         )
 
         if new_maze:
-            self.wall_list, self.coin_list, self.trap_list = create_maze()
+            self.wall_list, self.coin_list, self.trap_list = create_environment(game, size=size)
         else:
             self.wall_list = CUSTOM_WALL_LIST_5
-            self.coin_list = CUSTOM_COIN_LIST_5 
+            self.coin_list = CUSTOM_COIN_LIST_5.copy() 
             self.trap_list = CUSTOM_TRAP_LIST_5
 
         # print(self.maze)
@@ -227,6 +266,8 @@ class GridWorldEnv(gym.Env):
         self._target_location = np.asarray([self.size - 1, self.size - 1])
         # Choose the agent's location uniformly at random within the maze
         self._agent_location = np.asarray([0, 0]) # self.np_random.integers(0, self.size, size=2, dtype=int)
+        # self.coin_list = CUSTOM_COIN_LIST_5.copy()
+
         while self._agent_location.tolist() in self.wall_list or np.array_equal(self._target_location, self._agent_location):
             self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
 
@@ -338,12 +379,11 @@ class GridWorldEnv(gym.Env):
 
         for trap in self.trap_list:
             trap = np.asarray(trap)
-            pygame.draw.rect(
-                canvas,
-                (255, 0, 0),
-                pygame.Rect(
-                    trap * pix_square_size,
-                    (pix_square_size, pix_square_size),
+            pygame.draw.rect(canvas,
+                            (255, 0, 0),
+                            pygame.Rect(
+                                trap * pix_square_size,
+                                (pix_square_size, pix_square_size),
             )
         )
 
